c785038bf97723d553f6c9b20d9ccabbffae2702
rapson-martyn <martyn4700@gmail.com>
2020-10-21 00:50:57 +0100

    Added vertical parallax

--- Projects/Squared/Assets/Background/Parallax.cs
+++ Projects/Squared/Assets/Background/Parallax.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour { 
+public class Parallax : MonoBehaviour
+{ 
 
     private float length;
     private float startPos;
---
+++ Projects/Squared/Assets/Background/VerticalParallax.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalParallax : MonoBehaviour
+{
+
+    private float length;
+    private float width;
+    private float startPos;
+    public GameObject camera;
+    public float parallaxEffect;
+
+    public bool isRight = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.y;
+        width = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sectionTest = (camera.transform.position.y * (1 - parallaxEffect));   //Test for if current pos is outside middle section
+        float dist = (camera.transform.position.y * parallaxEffect);    //Distance to move
+
+        float xPos = 0;
+
+        if (isRight)
+        {
+            xPos = camera.GetComponent<Camera>().ViewportToWorldPoint(new Vector2(1,0)).x - (width/2);
+        }
+        else
+        {
+            xPos = camera.GetComponent<Camera>().ViewportToWorldPoint(new Vector2(0, 0)).x + (width/2);
+        }
+
+        transform.position = new Vector3(xPos, startPos + dist, transform.position.z);  //New position
+
+
+        
+
+        if (sectionTest > startPos + length)        //If past the right boundary
+        {
+            startPos += length;                     //Move startPos one section to the right 
+        }
+        else if (sectionTest < startPos - length)   //If past the right boundary
+        {
+            startPos -= length;                     //Move startPos one section to the left 
+        }
+    }
+}
