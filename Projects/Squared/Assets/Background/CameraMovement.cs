f881b6aaabca75b0e604d18ca9ac19c9ba6e6270
rapson-martyn <martyn4700@gmail.com>
2020-10-21 17:15:19 +0100

    Added Planar parallax scrolling

---
+++ Projects/Squared/Assets/Background/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float speed = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+        rb.AddForce(movement);
+    }
+}
---
+++ Projects/Squared/Assets/Background/PlanarParallax.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanarParallax : MonoBehaviour
+{
+
+    private float height;
+    private float width;
+    private Vector2 startPos;
+    public GameObject camera;
+    public float parallaxEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        width = GetComponent<SpriteRenderer>().bounds.size.x;
+        height = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sectionTestX = (camera.transform.position.x * (1 - parallaxEffect));   //Test for if current pos is outside middle section above
+        float sectionTestY = (camera.transform.position.y * (1 - parallaxEffect));   //Test for if current pos is outside middle section below
+        float distX = (camera.transform.position.x * parallaxEffect);    //Distance to move in X
+        float distY = (camera.transform.position.y * parallaxEffect);    //Distance to move in Y
+
+        transform.position = new Vector3(startPos.x + distX, startPos.y + distY, transform.position.z);  //New position
+
+        if (sectionTestX > startPos.x + width)        //If past the right boundary
+        {
+            startPos.x += width;                     //Move startPos one section to the right 
+        }
+        else if (sectionTestX < startPos.x - width)   //If past the right boundary
+        {
+            startPos.x -= width;                     //Move startPos one section to the left 
+        }
+
+        if (sectionTestY > startPos.y + height)        //If past the top boundary
+        {
+            startPos.y += height;                     //Move startPos one section up
+        }
+        else if (sectionTestY < startPos.y - height)   //If past the bottom boundary
+        {
+            startPos.y -= height;                     //Move startPos one section down 
+        }
+    }
+}
