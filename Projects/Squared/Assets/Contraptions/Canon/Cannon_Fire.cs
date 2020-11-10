e3583430a17eb81d0dbad76899617a734d3cf6e3
rapson-martyn <martyn4700@gmail.com>
2020-11-10 16:21:01 +0000

    Added a cannon
    Added Sprites for base and snout of cannon. Added scripts to handle firing and rotating

---
+++ Projects/Squared/Assets/Contraptions/Canon/Cannon_Fire.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon_Fire : MonoBehaviour
+{
+
+    [SerializeField] private float firingPower;
+    [SerializeField] private float delayTime = 1;
+    private Transform parent;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private float GetParentZRotation()
+    {
+        Debug.Log("Rotation of Parent: " + parent.rotation.eulerAngles.z);
+        return parent.rotation.eulerAngles.z;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.GetComponent<Rigidbody2D>() != null)
+        {
+            Rigidbody2D otherRigidbody2D = other.GetComponent<Rigidbody2D>();
+            Vector2 firingForce = new Vector2(0, firingPower);
+            firingForce = Quaternion.Euler(0, 0, GetParentZRotation()) * firingForce;
+
+            otherRigidbody2D.AddForce(firingForce);
+        } 
+        else
+        {
+            Debug.Log("No Rigidbody to fire in " + "Cannon_Fire" + " on " + gameObject.name);
+            
+        }
+    }
+
+
+}
---
+++ Projects/Squared/Assets/Contraptions/Canon/SimpleRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour
+{
+    Rigidbody2D rb;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb != null)
+        {
+            rb.AddTorque(0.1f);
+        }
+        else
+        {
+            Debug.Log("No RIGIDBODY2D at: " + gameObject.name + " in SCRIPT: " + this.name);
+        }
+    }
+}
