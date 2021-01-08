ec3da816612e6f10733e508b8ae00908bcb95517
Istwyth <66082813+Istwyth@users.noreply.github.com>
2021-01-08 22:10:35 +0000

    Added Dash and Jump Code
    Code needs a more experienced person to take a look at it but compiles - experimented with prefab pools ;)

---
+++ Projects/Platformer/Assets/Scripts/PlayerAfterImagePool.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAfterImagePool : MonoBehaviour
+{
+    [SerializeField]
+    
+    private GameObject afterImagePrefab;
+
+    private Queue<GameObject> availableObjects = new Queue<GameObject>(); // store the pool of unused player prefabs
+
+    public static PlayerAfterImagePool Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        GrowPool();
+
+    }
+
+    private void GrowPool() // create more game objects for the pool
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            var instanceToAdd = Instantiate(afterImagePrefab);
+            instanceToAdd.transform.SetParent(transform);
+            AddToPool(instanceToAdd);
+
+        }
+
+    }
+
+    public void AddToPool(GameObject instance)
+    {
+        instance.SetActive(false);
+        availableObjects.Enqueue(instance);
+    }
+    public GameObject GetFromPool()
+    {
+        if(availableObjects.Count == 0)
+        {
+            GrowPool();
+        }
+
+        var instance = availableObjects.Dequeue();
+        instance.SetActive(true);
+        return instance;
+    }
+
+}
--- Projects/Platformer/Assets/Scripts/PlayerController.cs
+++ Projects/Platformer/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,26 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed;
     public float jumpForce;
+    public Transform ceilingCheck;
+    public Transform groundCheck;
+    public LayerMask groundObjects;
+    public float checkRadius;
     
     private Rigidbody2D rb;
     private bool facingRight = true;
     private float moveDirection;
     private bool isJumping = false;
+    private bool isGrounded;
+
+    //Dash Variables
+    private bool isDashing;
+    public float dashTime;
+    public float dashSpeed;
+    public float distanceBetweenImages;
+    public float dashCoolDown;
+    private float dashTimeLeft;
+    private float lastImageXpos;
+    private float lastDash = -100f;
 
 
     // Awake is called before the Start method
@@ -24,11 +39,16 @@ public class PlayerController : MonoBehaviour
      // Update is called once per frame
     void Update()
     {
+            //Check if Player is grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
+
         //Get player inputs
         ProcessInputs();
 
         //Animate
         Animate();
+
+        CheckDash();
         
     }
 
@@ -41,9 +61,55 @@ public class PlayerController : MonoBehaviour
     private void ProcessInputs()
     {
         moveDirection = Input.GetAxis("Horizontal");// Scale being left -1, right 1
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
             isJumping = true;
+        }
+        if(Input.GetButtonDown("Dash"))
+        {
+            if(Time.time >= (lastDash + dashCoolDown))
+            
+            AttemptToDash();
+            
+            
+        }
+        
+    }
+
+    private void AttemptToDash()
+    {
+        isDashing = true;
+        dashTimeLeft = dashTime;
+        lastDash = Time.time;
+
+        PlayerAfterImagePool.Instance.GetFromPool();
+        lastImageXpos = transform.position.x;
+
+    }
+
+    private void CheckDash()
+    {
+        if(isDashing) // Checks if the player should be dashing
+        {
+            if(dashTimeLeft > 0)
+            {
+                
+                rb.velocity = new Vector2(dashSpeed * moveDirection, rb.velocity.y);
+                dashTimeLeft -= Time.deltaTime;
+
+                if(Mathf.Abs(transform.position.x - lastImageXpos) > distanceBetweenImages)
+                {
+                    PlayerAfterImagePool.Instance.GetFromPool();
+                    lastImageXpos = transform.position.x;
+                }
+            }
+            
+            if(dashTimeLeft <= 0)
+            {
+                isDashing = false;
+                
+            }
+
         }
     }
 
